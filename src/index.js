452bae1638420d11d4c2c52dd9e746ffa9cdd5e2
insert <94934670+slqntdevss@users.noreply.github.com>
2025-08-12 18:35:39 -0400

    add 404

--- src/index.js
+++ src/index.js
@@ -9,6 +9,14 @@ app.use(
 	express.static(join(dirname(fileURLToPath(import.meta.url)), "../public/"))
 );
 
+app.use((req, res, next) => {
+	res
+		.status(404)
+		.sendFile(
+			join(dirname(fileURLToPath(import.meta.url)), "../public/404.html")
+		);
+});
+
 app.listen(process.env.PORT, () => {
 	console.log(`Listening on port ${process.env.PORT}`);
 	console.log(`http://localhost:${process.env.PORT}`);
