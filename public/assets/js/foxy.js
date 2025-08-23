63727233239bcbc8d0aa2eb721305e62ac316949
insert <94934670+slqntdevss@users.noreply.github.com>
2025-08-23 01:42:32 -0400

    fix jumpscare and containers

--- public/assets/js/foxy.js
+++ public/assets/js/foxy.js
@@ -1,23 +1,25 @@
-document.body.addEventListener("click", () => {
-    if (Math.random() < 0.0001) {
-        const screen = document.createElement("div");
-        screen.className = "surprise";
+document.addEventListener("click", () => {
+	if (Math.random() < 0.0001) {
+		const screen = document.createElement("div");
+		screen.className = "surprise";
 
-        const pic = document.createElement("img");
-        pic.src = "assets/getscared.gif";
-        pic.className = "surprisemf";
+		const pic = document.createElement("img");
+		pic.src = "assets/getscared.gif";
+		pic.className = "surprisemf";
 
-        const sound = new Audio("assets/imaginegettingjumpscared.mp3");
-        sound.volume = 1.0;
-        sound.play();
+		const sound = new Audio("assets/imaginegettingjumpscared.mp3");
+		sound.volume = 1.0;
+		sound.play();
 
-        screen.appendChild(pic);
-        document.body.appendChild(screen);
+		screen.appendChild(pic);
+		document.body.appendChild(screen);
 
-        setTimeout(() => {
-            document.body.removeChild(screen);
-            sound.pause();
-            sound.currentTime = 0;
-        }, 2000 + Math.random() * 500);
-    }
-}, { once: true });
+		setTimeout(() => {
+			document.body.removeChild(screen);
+			sound.pause();
+			sound.currentTime = 0;
+		}, 2000 + Math.random() * 500);
+	} else {
+		console.log("safe for now........");
+	}
+});
