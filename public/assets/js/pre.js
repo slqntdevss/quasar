43ed10f252167151ab4e563b972749ffa682f9e6
insert <94934670+slqntdevss@users.noreply.github.com>
2025-11-17 22:57:06 -0500

    i think this might work

--- public/assets/js/pre.js
+++ public/assets/js/pre.js
@@ -1,205 +1,70 @@
 "use strict";
 
-document.addEventListener("DOMContentLoaded", () => {
-	document.title = "Quasar";
-	const title = document.querySelectorAll("#title");
-	const address = document.getElementById("address");
-	title.forEach((t) => (t.textContent = "Quasar"));
-	address.placeholder = "browse the internet";
-	const stockSW = "./sw.js";
-	const swAllowedHostnames = ["localhost", "127.0.0.1"];
-	const form = document.getElementById("form");
-	const connection = new BareMux.BareMuxConnection("/mux/worker.js");
-	const { ScramjetController } = $scramjetLoadController();
-	const autoc = document.getElementById("autoc");
-	const wContainer = document.querySelector(".w-container");
-	const backBtn = document.getElementById("backBtn");
-	const forwardBtn = document.getElementById("forwardBtn");
-	const reloadBtn = document.getElementById("reloadBtn");
-	const fullscreenBtn = document.getElementById("fullscreenBtn");
-	const closeBtn = document.getElementById("closeBtn");
-	let frame = document.getElementById("frame");
-	let timeout;
-	async function registerSW() {
-		if (!navigator.serviceWorker) {
-			if (
-				location.protocol !== "https:" &&
-				!swAllowedHostnames.includes(location.hostname)
-			)
-				throw new Error("Service workers cannot be registered without https.");
+const stockSW = "./sw.js";
+const swAllowedHostnames = ["localhost", "127.0.0.1"];
+const connection = new BareMux.BareMuxConnection("/mux/worker.js");
+const { ScramjetController } = $scramjetLoadController();
 
-			throw new Error("Your browser doesn't support service workers.");
-		}
+let timeout;
+async function registerSW() {
+	if (!navigator.serviceWorker) {
+		if (
+			location.protocol !== "https:" &&
+			!swAllowedHostnames.includes(location.hostname)
+		)
+			throw new Error("Service workers cannot be registered without https.");
 
-		await navigator.serviceWorker.register(stockSW);
+		throw new Error("Your browser doesn't support service workers.");
 	}
-	async function searchSJ(url) {
-		try {
-			await registerSW();
-		} catch (err) {
-			throw err;
-		}
 
-		let cleanedUrl = search(url, "https://duckduckgo.com/?q=%s");
-
-		if (cleanedUrl.includes("://now.gg")) {
-			cleanedUrl = "https://nowgg.fun";
-		}
-		frame.style.display = "block";
-		let wispUrl =
-			(location.protocol === "https:" ? "wss" : "ws") +
-			"://" +
-			location.host +
-			"/wisp/";
-
-		if ((await connection.getTransport()) !== "/ep/index.mjs") {
-			await connection.setTransport("/ep/index.mjs", [{ wisp: wispUrl }]);
-		}
-		const sjEncode = scramjet.encodeUrl.bind(scramjet);
-		frame.src = sjEncode(cleanedUrl);
-		cursor.style.opacity = 0;
-		document.documentElement.style.cursor = "auto";
-		document.body.style.cursor = "auto";
-		wContainer.classList.add("show");
-		autoc.classList.remove("show");
+	await navigator.serviceWorker.register(stockSW);
+}
+async function searchSJ(url) {
+	try {
+		await registerSW();
+	} catch (err) {
+		throw err;
 	}
-	function search(input, template) {
-		try {
-			return new URL(input).toString();
-		} catch (err) {}
 
-		try {
-			const url = new URL(`http://${input}`);
-			if (url.hostname.includes(".")) return url.toString();
-		} catch (err) {}
-		return template.replace("%s", encodeURIComponent(input));
+	let cleanedUrl = search(url, "https://duckduckgo.com/?q=%s");
+
+	if (cleanedUrl.includes("://now.gg")) {
+		cleanedUrl = "https://nowgg.fun";
 	}
-	const scramjet = new ScramjetController({
-		files: {
-			wasm: "/marcs/scramjet.wasm.wasm",
-			all: "/marcs/scramjet.all.js",
-			sync: "/marcs/scramjet.sync.js",
-		},
-	});
-	scramjet.init();
-	form.addEventListener("submit", async (event) => {
-		event.preventDefault();
-		searchSJ(address.value);
-	});
+	frame.style.display = "block";
+	let wispUrl =
+		(location.protocol === "https:" ? "wss" : "ws") +
+		"://" +
+		location.host +
+		"/wisp/";
 
-	frame.addEventListener("load", () => {
-		const url = scramjet.decodeUrl(frame.src);
-		document.getElementById("urlInput").value = url;
-	});
+	if ((await connection.getTransport()) !== "/ep/index.mjs") {
+		await connection.setTransport("/ep/index.mjs", [{ wisp: wispUrl }]);
+	}
+	const sjEncode = scramjet.encodeUrl.bind(scramjet);
+	frame.src = sjEncode(cleanedUrl);
+	cursor.style.opacity = 0;
+	document.documentElement.style.cursor = "auto";
+	document.body.style.cursor = "auto";
+	wContainer.classList.add("show");
+	autoc.classList.remove("show");
+}
+function search(input, template) {
+	try {
+		return new URL(input).toString();
+	} catch (err) {}
 
-	address.addEventListener("input", (e) => {
-		clearTimeout(timeout);
-		timeout = setTimeout(async () => {
-			const query = e.target.value.trim();
-			if (query.length > 0) {
-				try {
-					const response = await fetch(`/autoc?q=${encodeURIComponent(query)}`);
-					if (!response.ok) {
-						console.error("autocomplete request failed", response.status);
-						return;
-					}
-					const suggestions = await response.json();
-					console.log(suggestions);
-					autoc.innerHTML = "";
-					if (suggestions.length > 0) {
-						for (const suggestion of suggestions) {
-							const div = document.createElement("div");
-							div.classList.add("autoc-item");
-							div.textContent = suggestion.phrase;
-							div.addEventListener("click", () => {
-								address.value = suggestion.phrase;
-								form.requestSubmit();
-								autoc.classList.remove("show");
-							});
-							autoc.appendChild(div);
-						}
-						autoc.classList.add("show");
-					} else {
-						autoc.classList.remove("show");
-					}
-				} catch (err) {
-					console.log("autocomplete failed: " + err);
-				}
-			} else {
-				autoc.classList.remove("show");
-			}
-		});
-	});
-	backBtn.addEventListener("click", () => {
-		if (frame.contentWindow) {
-			frame.contentWindow.history.back();
-		}
-	});
-	forwardBtn.addEventListener("click", () => {
-		if (frame.contentWindow) {
-			frame.contentWindow.history.forward();
-		}
-	});
-	reloadBtn.addEventListener("click", () => {
-		frame.src = frame.src;
-	});
-	fullscreenBtn.addEventListener("click", () => {
-		if (!document.fullscreenElement) {
-			frame.requestFullscreen().catch((err) => {
-				console.error(`Error attempting to enable fullscreen: ${err.message}`);
-			});
-		} else {
-			document.exitFullscreen();
-		}
-	});
-	closeBtn.addEventListener("click", () => {
-		frame.src = "about:blank";
-		document.querySelector(".center").style.display = "flex";
-		document.querySelector(".w-container").classList.remove("show");
-		frame.style.display = "none";
-		cursor.style.opacity = 1;
-		document.documentElement.style.cursor = "none";
-		document.body.style.cursor = "none";
-	});
-	document.getElementById("urlForm").addEventListener("submit", async (e) => {
-		event.preventDefault();
-		searchSJ(document.getElementById("urlInput").value);
-	});
-
-	(async function () {
-		const grid = document.getElementById("quick-apps-grid");
-		if (!grid) return;
-		try {
-			const resp = await fetch("/assets/json/apps.json");
-			if (!resp.ok) {
-				grid.innerHTML =
-					'<div style="color: var(--main-text, white); text-align:center;">No apps found.</div>';
-				return;
-			}
-			const apps = await resp.json();
-			const gridHTML = `<div class="apps-grid-container">
-		${apps
-			.slice(0, 6)
-			.map(
-				(app, idx) => `
-			<div class="apps-grid-tile" tabindex="0" data-url="${app.url}">
-				<img src="/assets/img${app.img}" alt="${app.name}" />
-				<span class="apps-grid-label">${app.name}</span>
-			</div>
-		`
-			)
-			.join("")}
-		</div>`;
-			grid.innerHTML = gridHTML;
-			Array.from(grid.querySelectorAll(".apps-grid-tile")).forEach((tile) => {
-				tile.addEventListener("click", async () => {
-					let url = tile.getAttribute("data-url");
-					searchSJ(url);
-				});
-			});
-		} catch (e) {
-			grid.innerHTML =
-				'<div style="color: var(--main-text, white); text-align:center;">Error loading apps.</div>';
-		}
-	})();
+	try {
+		const url = new URL(`http://${input}`);
+		if (url.hostname.includes(".")) return url.toString();
+	} catch (err) {}
+	return template.replace("%s", encodeURIComponent(input));
+}
+const scramjet = new ScramjetController({
+	files: {
+		wasm: "/marcs/scramjet.wasm.wasm",
+		all: "/marcs/scramjet.all.js",
+		sync: "/marcs/scramjet.sync.js",
+	},
 });
+scramjet.init();
