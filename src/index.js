e5d6c2c582c7a4e46631583ca90bced707b8700b
insert <94934670+slqntdevss@users.noreply.github.com>
2025-12-21 19:45:16 -0500

    oops

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { scramjetPath } from "@mercuryworkshop/scramjet/path";
 import { epoxyPath } from "@mercuryworkshop/epoxy-transport";
 import { baremuxPath } from "@mercuryworkshop/bare-mux/node";
 import { libcurlPath } from "@mercuryworkshop/libcurl-transport";
-import { server as wisp, logging } from "@mercuryworkshop/wisp-js/server";
+//import { server as wisp, logging } from "@mercuryworkshop/wisp-js/server";
 
 const app = express();
 
@@ -22,7 +22,7 @@ const publicDir = path.join(
 	`../public`
 );
 
-logging.set_level(logging.NONE);
+//logging.set_level(logging.NONE);
 
 const server = createServer(app);
 
@@ -44,13 +44,13 @@ app.use((req, res, next) => {
 	next();
 });
 
-server.on("upgrade", (req, socket, head) => {
+/*server.on("upgrade", (req, socket, head) => {
 	if (req.url.endsWith("/wisp/")) {
 		wisp.routeRequest(req, socket, head);
 	} else {
 		socket.end();
 	}
-});
+});*/
 
 app.use((req, res, next) => {
 	const isDir = req.path.endsWith("/");
