becec2ae88404e99252c2511137745f66e55e8a2
insert <94934670+slqntdevss@users.noreply.github.com>
2026-01-11 10:44:45 -0500

    whoops

--- public/assets/js/ads.js
+++ public/assets/js/ads.js
@@ -116,3 +116,10 @@ window.setTimeout = function (fn, delay) {
 	}
 	return originalSetTimeout.apply(this, arguments);
 };
+
+setTimeout(() => {
+	loadAds({
+		banner: "94d3e6b189169213c968a0f35cf2c24b",
+		native: "976e351ff44eac06013f3d88e10200d0",
+	});
+}, 2000);
