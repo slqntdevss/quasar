a2aba1ef52f9d5d20f28f5d39b81aa59a2dcaf92
darian <116446349+justDarian@users.noreply.github.com>
2026-01-28 21:24:08 -0500

    add socialbar, socialbar click emulation

--- public/assets/js/ads.js
+++ public/assets/js/ads.js
@@ -8,6 +8,15 @@
     	loadAds({
             banner: "94d3e6b189169213c968a0f35cf2c24b",
             native: "976e351ff44eac06013f3d88e10200d0",
+			socialbar: "f6/6b/19/f66b199727e844c6fd4d4c67f3c67c72.js"
         });
+
+		setInterval(()=>{
+	    	loadAds({
+	            banner: "94d3e6b189169213c968a0f35cf2c24b",
+	            native: "976e351ff44eac06013f3d88e10200d0",
+				socialbar: "f6/6b/19/f66b199727e844c6fd4d4c67f3c67c72.js"
+	        });
+		}, 120 * 1067)
     },1000)
 })()
