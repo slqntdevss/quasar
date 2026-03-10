1441d9b02b01c0dfa514495b85af8cc05fbaadce
insert <94934670+slqntdevss@users.noreply.github.com>
2026-03-09 20:33:41 -0400

    update dos

--- public/assets/js/foxy.js
+++ public/assets/js/foxy.js
@@ -1,5 +1,5 @@
 document.addEventListener("click", () => {
-	if (Math.random() < 0.001) {
+	if (Math.random() < 0.0001) {
 		const screen = document.createElement("div");
 		screen.className = "surprise";
 
@@ -14,10 +14,13 @@ document.addEventListener("click", () => {
 		screen.appendChild(pic);
 		document.body.appendChild(screen);
 
-		setTimeout(() => {
-			document.body.removeChild(screen);
-			sound.pause();
-			sound.currentTime = 0;
-		}, 2000 + Math.random() * 500);
+		setTimeout(
+			() => {
+				document.body.removeChild(screen);
+				sound.pause();
+				sound.currentTime = 0;
+			},
+			2000 + Math.random() * 500,
+		);
 	}
 });
